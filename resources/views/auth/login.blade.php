fe624cc67e7ff14758d91af2b1757f882f091867
Leandro dos Santos <lds.leosantos@gmail.com>
2022-11-26 15:38:14 -0300

    Ajuste template auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,26 +1,6 @@
-<!doctype html>
-<html lang="en">
+@extends('auth.auth_layout.auth_master')
 
-<head>
-
-    <meta charset="utf-8" />
-    <title>Login | Upcube Admin</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesdesign" name="author" />
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }} ">
-
-    <!-- Bootstrap Css -->
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-    <!-- Icons Css -->
-    <link href="{{ asset('assets/css/icons.min.css') }} " rel="stylesheet" type="text/css" />
-    <!-- App Css-->
-    <link href=" {{ asset('assets/css/app.min.css') }} " id="app-style" rel="stylesheet" type="text/css" />
-
-</head>
-
-<body class="auth-body-bg">
+@section('content')
     <div class="bg-overlay"></div>
     <div class="wrapper-page">
         <div class="container-fluid p-0">
@@ -45,8 +25,8 @@
                             @csrf
                             <div class="form-group mb-3 row">
                                 <div class="col-12">
-                                    <input class="form-control" id="username" name="username" type="text"
-                                        required="" placeholder="Username">
+                                    <input class="form-control" id="username" name="username" type="text" required=""
+                                        placeholder="Username">
                                 </div>
                             </div>
 
@@ -92,17 +72,4 @@
         </div>
         <!-- end container -->
     </div>
-    <!-- end -->
-
-    <!-- JAVASCRIPT -->
-    <!-- JAVASCRIPT -->
-    <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
-    <script src="{{ asset('assets/js/app.js') }}"></script>
-
-</body>
-
-</html>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,26 +1,6 @@
-<!doctype html>
-<html lang="en">
+@extends('auth.auth_layout.auth_master')
 
-<head>
-
-    <meta charset="utf-8" />
-    <title>Register | Upcube | Admin</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesdesign" name="author" />
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }} ">
-
-    <!-- Bootstrap Css -->
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-    <!-- Icons Css -->
-    <link href="{{ asset('assets/css/icons.min.css') }} " rel="stylesheet" type="text/css" />
-    <!-- App Css-->
-    <link href=" {{ asset('assets/css/app.min.css') }} " id="app-style" rel="stylesheet" type="text/css" />
-
-</head>
-
-<body class="auth-body-bg">
+@section('content')
     <div class="bg-overlay"></div>
     <div class="wrapper-page">
         <div class="container-fluid p-0">
@@ -30,10 +10,10 @@
                     <div class="text-center mt-4">
                         <div class="mb-3">
                             <a href="index.html" class="auth-logo">
-                                <img src="{{asset('assets/images/logo-dark.png')}} " height="30" class="logo-dark mx-auto"
-                                    alt="">
-                                <img src=" {{asset('assets/images/logo-light.png')}} " height="30" class="logo-light mx-auto"
-                                    alt="">
+                                <img src="{{ asset('assets/images/logo-dark.png') }} " height="30"
+                                    class="logo-dark mx-auto" alt="">
+                                <img src=" {{ asset('assets/images/logo-light.png') }} " height="30"
+                                    class="logo-light mx-auto" alt="">
                             </a>
                         </div>
                     </div>
@@ -41,21 +21,24 @@
                     <h4 class="text-muted text-center font-size-18"><b>Register</b></h4>
 
                     <div class="p-3">
-                         <form method="POST"  class="form-horizontal mt-3" action="{{ route('register') }}">
+                        <form method="POST" class="form-horizontal mt-3" action="{{ route('register') }}">
                             @csrf
-                              <div class="form-group mb-3 row">
+                            <div class="form-group mb-3 row">
                                 <div class="col-12">
-                                    <input class="form-control" id="name" name="name" type="text" required="" placeholder="You Best Name">
-                                </div>
-                            </div>
-                               <div class="form-group mb-3 row">
-                                <div class="col-12">
-                                    <input class="form-control" id="username" name="username" type="text" required="" placeholder="Username">
+                                    <input class="form-control" id="name" name="name" type="text" required=""
+                                        placeholder="You Best Name">
                                 </div>
                             </div>
                             <div class="form-group mb-3 row">
                                 <div class="col-12">
-                                    <input class="form-control" name="email" id="email" type="email" required="" placeholder="Email">
+                                    <input class="form-control" id="username" name="username" type="text" required=""
+                                        placeholder="Username">
+                                </div>
+                            </div>
+                            <div class="form-group mb-3 row">
+                                <div class="col-12">
+                                    <input class="form-control" name="email" id="email" type="email" required=""
+                                        placeholder="Email">
                                 </div>
                             </div>
 
@@ -63,13 +46,15 @@
 
                             <div class="form-group mb-3 row">
                                 <div class="col-12">
-                                    <input class="form-control" id="password" name="password" type="password" required="" placeholder="Password">
+                                    <input class="form-control" id="password" name="password" type="password"
+                                        required="" placeholder="Password">
                                 </div>
                             </div>
 
-                                <div class="form-group mb-3 row">
+                            <div class="form-group mb-3 row">
                                 <div class="col-12">
-                                    <input class="form-control" id="password_confirmation" name="password_confirmation" type="password" required="" placeholder="Password Confirmation">
+                                    <input class="form-control" id="password_confirmation" name="password_confirmation"
+                                        type="password" required="" placeholder="Password Confirmation">
                                 </div>
                             </div>
 
@@ -90,7 +75,7 @@
 
                             <div class="form-group mt-2 mb-0 row">
                                 <div class="col-12 mt-3 text-center">
-                                    <a href="{{route('login')}}" class="text-muted">Already have account?</a>
+                                    <a href="{{ route('login') }}" class="text-muted">Already have account?</a>
                                 </div>
                             </div>
                         </form>
@@ -104,16 +89,4 @@
         <!-- end container -->
     </div>
     <!-- end -->
-
-
-    <!-- JAVASCRIPT -->
-    <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
-    <script src="{{ asset('assets/js/app.js') }}"></script>
-
-</body>
-
-</html>
+@endsection
