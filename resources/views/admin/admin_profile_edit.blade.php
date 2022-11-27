edc9d3ad6590ff7ed1ab87ff3f569b90d98a102c
Leandro dos Santos <lds.leosantos@gmail.com>
2022-11-27 11:34:31 -0300

    edit image profile

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -10,29 +10,44 @@
 
                             <h4 class="card-title">Edit Profile Page</h4>
 
-                            <form class="" action=""  >
+                            <form class="" action="">
                                 <div class="row mb-3">
                                     <label for="name" class="col-sm-2 col-form-label">Name</label>
                                     <div class="col-sm-10">
-                                        <input class="form-control" type="text" placeholder="Name"
-                                            id="name" name="name" value="{{$editData->name}}" >
+                                        <input class="form-control" type="text" placeholder="Name" id="name"
+                                            name="name" value="{{ $editData->name }}">
                                     </div>
                                 </div>
-                                  <div class="row mb-3">
+                                <div class="row mb-3">
                                     <label for="email" class="col-sm-2 col-form-label">E-mail</label>
                                     <div class="col-sm-10">
-                                        <input class="form-control" type="email" placeholder="E-mail"
-                                            id="email" name="email" value="{{$editData->email}}">
+                                        <input class="form-control" type="email" placeholder="E-mail" id="email"
+                                            name="email" value="{{ $editData->email }}">
                                     </div>
                                 </div>
 
-                                  <div class="row mb-3">
+                                <div class="row mb-3">
                                     <label for="username" class="col-sm-2 col-form-label">Username</label>
                                     <div class="col-sm-10">
-                                        <input class="form-control" type="text" placeholder="Username"
-                                            id="username" name="username" value="{{$editData->username}}">
+                                        <input class="form-control" type="text" placeholder="Username" id="username"
+                                            name="username" value="{{ $editData->username }}">
                                     </div>
                                 </div>
+                                <div class="row mb-3">
+                                    <label for="profile_image" class="col-sm-2 col-form-label">Profile Image</label>
+                                    <div class="col-sm-10">
+                                        <input class="form-control" type="file" id="profile_image" name="profile_image">
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="profile_image" class="col-sm-2 col-form-label"> New Image</label>
+                                    <div class="col-sm-10">
+                                        <img class="rounded avatar-lg"
+                                            src="{{ asset('assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                    </div>
+                                </div>
+                                <input type="submit" value="Update Profile" name="submit" class="btn btn-info waves-effect waves-light">
                             </form>
                         </div>
                     </div>
