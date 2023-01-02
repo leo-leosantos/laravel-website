f28dad414583f10988926ed2b938c39fde03ee28
Informatica ESPMG <informatica@esp.mg.gov.br>
2023-01-02 16:30:32 -0300

    Seção 10 - Aula 75 - Parte 7 da configuração da pagina do blog back end

--- app/Http/Controllers/Home/BlogController.php
+++ app/Http/Controllers/Home/BlogController.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Home;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 use App\Models\Blog;
 use App\Models\BlogCategory;
-
 use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 
 class BlogController extends Controller
 {
 
+    public $blogs, $categories;
 
     public function AllBlog()
     {
@@ -35,38 +35,116 @@ class BlogController extends Controller
             'blog_description' => 'required',
             'blog_image' => 'required',
 
-            ],[
-                'blog_category_id.required' =>  'Blog Category is Required',
-                'blog_title.required' =>  'Blog Title is Required',
-                'blog_tags.required' =>  'Blog Tags is Required',
-                'blog_description.required' =>  'Blog description is Required',
-                'blog_image.required' =>  'Blog Image is Required',
+        ], [
+            'blog_category_id.required' =>  'Blog Category is Required',
+            'blog_title.required' =>  'Blog Title is Required',
+            'blog_tags.required' =>  'Blog Tags is Required',
+            'blog_description.required' =>  'Blog description is Required',
+            'blog_image.required' =>  'Blog Image is Required',
 
-            ]);
-    
-                $image = $request->file('blog_image');
-    
-                $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
-    
-                $image_save = Image::make($image)->resize(430,327)->save('upload/blog/'.$name_gen);
-    
-                $save_url = 'upload/blog/' . $name_gen;
-    
-    
-                Blog::insert([
-                    'blog_category_id' => $request->blog_category_id,
-                    'blog_title' => $request->blog_title,
-                    'blog_tags' => $request->blog_tags,
-                    'blog_description' => $request->blog_description,
-                    'blog_image' => $save_url,
-                    'created_at' => Carbon::now()
-                ]);
-    
-                $notification = [
-                    'message' => 'Blog Inserted Successfully',
-                    'alert-type' => 'success'
-                ];
-                return redirect()->route('all.blog')->with($notification);
+        ]);
+
+        $image = $request->file('blog_image');
+
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+
+        $image_save = Image::make($image)->resize(430, 327)->save('upload/blog/' . $name_gen);
+
+        $save_url = 'upload/blog/' . $name_gen;
+
+
+        Blog::insert([
+            'blog_category_id' => $request->blog_category_id,
+            'blog_title' => $request->blog_title,
+            'blog_tags' => $request->blog_tags,
+            'blog_description' => $request->blog_description,
+            'blog_image' => $save_url,
+            'created_at' => Carbon::now()
+        ]);
+
+        $notification = [
+            'message' => 'Blog Inserted Successfully',
+            'alert-type' => 'success'
+        ];
+        return redirect()->route('all.blog')->with($notification);
     }
 
+    public function EditBlog($id)
+    {
+        $blog = Blog::findOrFail($id);
+        $categories = BlogCategory::orderBy('blog_category', 'ASC')->get();
+        return view('admin.blogs.blogs_edit', compact('blog', 'categories'));
+    }
+
+    public function UpdateBlog(Request $request, $id)
+    {
+
+        if ($request->file('blog_image')) {
+            $image = $request->file('blog_image');
+
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+
+            $image_save = Image::make($image)->resize(430,327)->save('upload/blog/' . $name_gen);
+
+            $save_url = 'upload/blog/' . $name_gen;
+
+
+            Blog::findOrFail($id)->update([
+                'blog_category_id' => $request->blog_category_id,
+                'blog_title' => $request->blog_title,
+                'blog_tags' => $request->blog_tags,
+                'blog_description' => $request->blog_description,
+                'blog_image' => $save_url,
+                'updated_at' => Carbon::now()
+            ]);
+
+            $notification = [
+                'message' => 'Blog updated with Image Successfully',
+                'alert-type' => 'success'
+            ];
+            return redirect()->route('all.blog')->with($notification);
+
+        }else{
+            Blog::findOrFail($id)->update([
+                'blog_category_id' => $request->blog_category_id,
+                'blog_title' => $request->blog_title,
+                'blog_tags' => $request->blog_tags,
+                'blog_description' => $request->blog_description,
+                'updated_at' => Carbon::now()
+            ]);
+
+            $notification = [
+                'message' => 'Blog updated without Image Successfully',
+                'alert-type' => 'success'
+            ];
+            return redirect()->route('all.blog')->with($notification);
+        }//end else
+
+
+    }
+
+    public function DeleteBlog($id)
+    {
+        $blog = Blog::findOrFail($id);
+        $img = $blog->blog_image;
+
+        if ($blog) {
+            if (file_exists($img)) {
+                unlink($img);
+            }
+
+            Blog::findOrFail($id)->delete();
+            $notification = [
+                'message' => 'Blog Image deleted Successfully',
+                'alert-type' => 'success'
+            ];
+            return redirect()->route('all.blog')->with($notification);
+        }
+        $notification = [
+            'message' => 'Blog not deleted Successfully',
+            'alert-type' => 'error'
+        ];
+        return redirect()->route('all.blog')->with($notification);
+
+    }
 }
---
+++ resources/views/admin/blogs/blogs_edit.blade.php
@@ -0,0 +1,119 @@
+@extends('admin.admin_master')
+@section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<style type="text/css">
+    .bootstrap-tagsinput .tag{
+        margin-right: 2px;
+        color: #b70000;
+        font-weight: 700px;
+    } 
+</style>
+<div class="page-content">
+    <div class="container-fluid">
+
+         <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <h4 class="card-title">Edit Blog Page</h4>
+
+                        <form class="form-group" method="post"
+                            action="{{route('update.blog', $blog->id)}}"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <div class="row mb-3">
+                                <label for="portfolio-name" class="col-sm-2
+                                    col-form-label">Blog Category Name</label>
+                                <div class="col-sm-10">
+                                    <select name="blog_category_id" class="form-select" aria-label="Default select example">
+                                        <option selected="">Open this select menu</option>
+                                            @foreach ( $categories as $cat )
+                                            <option value="{{ $cat->id }}" {{ $cat->id == $blog->blog_category_id ? 'selected' : '' }}>{{ $cat->blog_category }}</option>
+                                            @endforeach
+                                    </select>
+                                  
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="blog_title" class="col-sm-2
+                                    col-form-label">Blog Title</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" type="text"
+                                        placeholder="Blog Title" id="blog_title"
+                                        name="blog_title" value=" {{old('blog_title') ?? $blog->blog_title}}"
+                                    >
+                                    @error('blog_title')
+                                    <span class="text-danger">{{$message}}</span>
+                                @enderror
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="blog-tags" class="col-sm-2
+                                    col-form-label">Blog Tags</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" type="text"
+                                        id="blog-tags"  data-role="tagsinput"
+                                        name="blog_tags" value="{{ $blog->blog_tags }}"
+                                    >
+                                    @error('blog_tags')
+                                    <span class="text-danger">{{$message}}</span>
+                                @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="blog_description" class="col-sm-2
+                                    col-form-label">Blog Description</label>
+                                <div class="col-sm-10">
+                                    <textarea class="form-control"
+                                      name="blog_description" id="elm1"  rows="3">
+                                        {{ $blog->blog_description }}
+                                    </textarea>
+                                    @error('blog_description')
+                                    <span class="text-danger">{{$message}}</span>
+                                @enderror
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="blog_image" class="col-sm-2
+                                    col-form-label">Blog Image</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" type="file"
+                                        id="image" name="blog_image">
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="showImage" class="col-sm-2
+                                    col-form-label"> New Image</label>
+                                <div class="col-sm-10">
+
+                                    <img class="rounded avatar-lg"
+                                        src="{{ asset($blog->blog_image) ??  url('upload/no_image.jpg') }}"
+                                    alt="" id="showImage">
+                                </div>
+                            </div>
+                            <input type="submit" value="Update Blog Page"
+                                name="submit" class="btn btn-info waves-effect
+                                waves-light">
+                        </form>
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+
+    $(document).ready(function() {
+            $('#image').change(function(e) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#showImage').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            });
+    });
+</script>
+@endsection
